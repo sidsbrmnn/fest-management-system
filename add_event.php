4ace67e462e52a063976a7cd267e92e327313074
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-11-21 00:01:07 +0530

    Refactoring code

--- add_event.php
+++ add_event.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['user_id'])) {
-    header('Location: events.php');
+    header('Location: login.php?back=add_event.php');
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -32,25 +32,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Add Event - Fest Management</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <?php include 'includes/_links.php'; ?>
+
 </head>
 
 <body>
-    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
-        <div class="container">
-            <a href="index.php" class="navbar-brand">Fest Management</a>
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
-                    <li class="nav-item"><a href="events.php" class="nav-link">Events</a></li>
-                </ul>
-                <a href="logout.php" class="btn btn-outline-light btn-sm">Log out</a>
-            </div>
-        </div>
-    </nav>
+    <?php include 'includes/_navbar.php'; ?>
 
-    <div class="container py-5">
+    <div class="container py-5" style="position: relative;">
         <h3>Add Event</h3>
         <div class="row mt-4">
             <div class="col-lg-6 col-12">
@@ -90,24 +79,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     </div>
                     <button type="submit" class="btn btn-primary">Add</button>
                 </form>
-                <?php if (isset($error_message)) { ?>
-                <small class="d-block text-danger mt-2">
-                    <?php echo $error_message; ?>
-                </small>
-                <?php } ?>
             </div>
         </div>
+
+        <?php include 'includes/_error_toast.php'; ?>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
+    <?php include 'includes/_scripts.php'; ?>
 </body>
 
 </html>
