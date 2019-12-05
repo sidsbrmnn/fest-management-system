583c0f095aa150c1fb5fa70dba1cedfb6d37d7d1
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-12-05 15:49:43 +0530

    toast delay and title font style modified

--- includes/_error_toast.php
+++ includes/_error_toast.php
@@ -1,7 +1,7 @@
 <?php if (isset($error_message)) { ?>
-<div class="toast" style="position: absolute; top: 1.5rem; right: 0;" data-delay="2000">
+<div class="toast" style="position: absolute; top: 1.5rem; right: 0;" data-delay="2500">
     <div class="toast-header">
-        <strong class="mr-auto text-danger">Error</strong>
+        <span class="mr-auto font-weight-semi-bold text-danger">Error</span>
         <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
             <i class="fas fa-times fa-xs"></i>
         </button>
